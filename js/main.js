fbe6b2e5f53274753d8e47c181af1b0685c5d374
Molotovs <henrikn@live.se>
2015-01-20 16:07:42 +0100

    Added .less compatibility and first db-dump. Started following along  a php oop Login/Register system tutorial by phpacademy on youtube.com

--- js/main.js
+++ js/main.js
@@ -1,8 +1,29 @@
-$(function() {
+$(document).ready(function() {
 
 
 
-  $("ul").children().addClass("btn btn-default");
+  $('li li a').addClass("btn btn-default");
  
-
+  $('article').addClass("box_outline");
+  $('p').addClass("col-md-offset-1");
 });
+
+
+// jquery live, gör inte såhär!!!
+// $("class").live("click",function(){});
+
+//setTimeour(function(){
+// 	$('<button class="class">knapp</button>').appendTo('body');
+// },3000);
+
+// jquery on, dis is the way!!
+//du kan göra den snabbare genom att vara snävare i din
+// första selector än att skriva document
+//$(document).on("click", "button.class", function(){
+	// 	addClass.("bogeli");
+	// });
+
+//htaccess gå till index oberoende på url.,.,..
+//history.pushState().................. :,(
+
+	//pageName = pageName || "start";
