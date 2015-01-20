4539efdd95f568931e416570c5bd32b8d2ca59b1
Molotovs <henrikn@live.se>
2015-01-20 20:32:19 +0100

    Still on step seven, did the step but getting an error: Fatal error: Call to private DB::__construct() from invalid context in D:\Program Files\Ampps\www\hn_webproduktion_bootstrap\php\index.php on line 5
    syntax maybe?

--- php/classes/db.php
+++ php/classes/db.php
@@ -1,1 +1,26 @@
 <?php
+class DB {
+	private static $_instance = null;
+	private
+	 $_pdo,
+	 $_query, 
+	 $_error = false,
+	 $_results,
+	 $_count = 0;
+
+	 private function __construct() {
+	 	try {
+	 		$this->_pdo = new PDO('mysql:host=' . config::get('mysql/host') . ';dbname=y' . config::get('mysql/db'), config::get('mysql/username'),config::get('mysql/password'));
+	 			echo 'Connected';
+	 	}	catch(PDOException $e) {
+	 			die($e->getMessage());
+	 	}
+	 }
+
+	 public static function getInstance() {
+	 	if(!isset(self::$_instance)) {
+	 		self::$_instance = new DB();
+	 	}
+	 	return self::$_instance;
+	 }
+}
--- php/index.php
+++ php/index.php
@@ -1,4 +1,5 @@
 <?php
 require_once 'core/init.php';
 
-echo config::get('mysql/host');
+
+$db = new DB();
